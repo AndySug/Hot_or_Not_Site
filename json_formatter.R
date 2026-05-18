ad73d848a3053d22ef44cf7ca9dd2a7f672e9d41
Andy Sugden <38018983+AndySug@users.noreply.github.com>
2026-05-18 12:47:49 +0100

    First complete R file

--- json_formatter.R
+++ json_formatter.R
@@ -2,7 +2,13 @@ setwd('C:/Users/AndyS/Documents/Hot or Not/hot_or_not_website/')
 libs<-c('tidyverse','tidyr','lubridate','googlesheets4','factoextra','corrplot','clipr','showtext','emojifont', 'scales','svglite','stringdist','jsonlite')
 lapply(libs,require,character.only=TRUE)
 font_add_google("Nunito Sans","nunito")
-font_add("noto-emoji","C:\\Users\\AndyS\\Documents\\R\\win-library\\4.1\\emojifont\\emoji_fonts\\NotoEmoji.ttf")
+
+data_26 <- read_csv('raw_data/data26.csv') %>%
+  rename(item = name) %>%
+  mutate(year = 2026, year_int = 7) %>%
+  pivot_longer(4:150) %>%
+  filter(item != 'weight' & item != 'referrer' & item != 'Hand' & item != 'Gender') %>%
+  select(1,4,5,6,7)
 
 data_25 <- read_csv('raw_data/data25.csv') %>% 
   mutate(year = 2025, year_int = 6) %>%
@@ -40,51 +46,103 @@ data_20 <- read_csv('raw_data/data20.csv',
   mutate(year = 2020, year_int = 1) %>%
   pivot_longer(2:10)
 
-data_all_years <- data_25 %>%
-  rbind(data_24, data_23, data_22, data_21, data_20) %>%
-  slice_sample(prop = 1)
+#Vote data final table
+data_all_years <- data_26 %>%
+  rbind(data_25, data_24, data_23, data_22, data_21, data_20) %>%
+  slice_sample(prop = 1) %>%
+  mutate(simplified_score = ifelse(as.numeric(value) >= 10, 10, ifelse(as.numeric(value) <= 1, 1, as.numeric(value))))
+
+write_csv(data_all_years,'./raw_data/all_scores.csv')
+
+simplified_all_time <- read_csv('./raw_data/all_time_26.csv')
+
+simplified_all_time <-
+  simplified_all_time %>%
+  mutate(living_human = case_when(grepl('?',`Living Human`,fixed = TRUE) ~ "Debateable",
+                                  grepl('y',`Living Human`,ignore.case = TRUE) ~ "Yes",
+                                  TRUE ~ "No"))
 
 data_summ <- data_all_years %>%
-  group_by(item, year, year_int, value) %>%
+  group_by(item, year, year_int, simplified_score) %>%
   summarise(count = n())
 
 score_hist <- data_summ %>% 
   pivot_wider(id_cols = c('item','year','year_int'),
-              names_from = value,
+              names_from = simplified_score,
               values_from = count,
               values_fill = 0)
 
-score_hist %>%
-  select(1:3) %>%
-  write_clip()
+score_nest <- score_hist %>%
+  nest(4:14)
 
-fixed_loc_to_25 <- read_csv('raw_data/fixed_loc.csv') %>%
-  transmute(item = Name,
-            year = Year,
-            score = Score,
-            tags = `Same Thing?`,
-            loc = loc)
+#Matching to images
+all_img <- list.files('C:/Users/AndyS/Documents/Hot or Not/hot_or_not_website/raw_image/',recursive=TRUE)
 
-fixed_loc_to_25 <- fixed_loc_to_25 %>% 
-  filter(item != 'Josh Gill (transparent PNG).png') %>%
-  filter(item != 'Greg Wallace') %>%
-  filter(item != 'Gregg Wallace (Hair)') %>%
-  filter(item != 'Leader of the Opposition Keir Starmer') %>%
-  filter(!(str_ends(loc,'gif'))) %>%
+no_import <- data.frame(loc = all_img) %>%
+  filter(!(str_ends(loc,'.import'))) %>%
+  mutate(Year = as.integer(substr(loc,1,4)))
+
+item_year <- simplified_all_time %>%
+  filter(is.na(Personal)) %>%
+  select(1,6)
+
+top_combo <- item_year %>%
+  left_join(no_import,relationship='many-to-many') %>%
+  mutate(dist = stringdist(Name,loc,method='lv'),
+         list_loc = paste0('./raw_image/',loc)) %>%
+  arrange(Year,Name,dist) %>%
+  group_by(Year,Name) %>%
+  slice_head(n=1) %>%
+  ungroup()
+
+loc_changes <- read_csv('./raw_data/loc_changes.csv') %>%
+  mutate(Year = as.integer(substr(loc,1,4)),
+         dist = NA,
+         list_loc = paste0('./raw_image/',loc))
+
+correct_combo <- top_combo %>%
+  anti_join(loc_changes,by=c('Name', 'Year')) %>%
+  rbind(loc_changes)
+
+correct_combo <- correct_combo %>% 
+  filter(Name != 'Josh Gill (transparent PNG).png') %>%
+  filter(Name != 'Greg Wallace') %>%
+  filter(Name != 'Gregg Wallace (Hair)') %>%
+  filter(Name != 'Leader of the Opposition Keir Starmer') %>%
+  filter() %>%
   mutate(site_loc = paste0('./raw_image/',loc))
 
-### Add 2026 in here
 
-### Need to fix a bunch of missing things as well
 
-loc_and_scores <- fixed_loc_to_25 %>%
-  inner_join(score_nest)
 
-write_csv(loc_and_scores,'./site_data/full_dataset.csv')
 
-json <- toJSON(loc_and_scores)
+nest_and_slug <- score_nest %>%
+  mutate(slug = tolower(gsub('[[:blank:]]+','-',gsub('[[:punct:]]+','',item))),
+         url = paste0('/',year,'/',slug))
+
+
+#Joining
+name_changes <- read_csv('./raw_data/name_changes.csv')
+
+scores_and_overall <- simplified_all_time %>%
+  left_join(name_changes,by=c("Name"="official_name","Year"="year")) %>%
+  mutate(matching_name = coalesce(yearly_name,Name)) %>%
+  left_join(nest_and_slug,by=c("matching_name"="item","Year"="year"))
+
+overall_w_image <- scores_and_overall %>%
+  inner_join(correct_combo,by=c("Name","Year")) %>%
+  select(-5,-8,-10,-12)
+
+write_csv(overall_w_image,'./site_data/full_dataset.csv')
+
+json <- toJSON(overall_w_image)
 
 write_file(json,"./site_data/full_dataset.json")
+
+
+#Missing - Pacha (new groove) & Chicken Doughnut Dippers
+
+
 
 
 
